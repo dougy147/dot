cd22d619e4c9a16dfc0a9cd246b2d52c06b5201e
dougy147 <drouillet.luc@gmail.com>
2022-03-27 10:54:01 +0200

    dark theme default test

--- .local/src/dmenu-light/config.h
+++ .local/src/dmenu-light/config.h
@@ -12,8 +12,8 @@ static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#474554" },
+	[SchemeNorm] = { "#4e4e4e", "#ffffff" },
+	[SchemeSel] = { "#000000", "#d0d0d0" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 static const unsigned int alphas[SchemeLast][2] = {
--- .local/src/dwm-light/config.h
+++ .local/src/dwm-light/config.h
@@ -17,15 +17,14 @@ static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static char *fonts[]          = { "monospace:size=12", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
 
-/* Dark Theme */
-static char normbgcolor[]           = "#222222";
-static char normbordercolor[]       = "#444444";
-static char normfgcolor[]           = "#bbbbbb";
-static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#ffffff";
-static char selbgcolor[]            = "#474554";
-
-
+/* Light Theme */
+static char normbgcolor[]           = "#ffffff";
+static char normbordercolor[]       = "#d0d0d0";
+static char normfgcolor[]           = "#000000";
+static char selfgcolor[]            = "#000000";
+static char selbordercolor[]        = "#000000";
+/*static char selbgcolor[]            = "#dadde2";*/
+static char selbgcolor[]            = "#d0d0d0";
 
 static char *colors[][3] = {
        /*               fg           bg           border   */
